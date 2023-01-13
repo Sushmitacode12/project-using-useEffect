7fe35c9d69add8859652f0592707aa0b32294966
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-01-13 16:33:30 +0530

    Rules of Hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/UI/store/auth-context';
 
+
 function App() {
-  
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedinInformation = localStorage.getItem('isLoggedIn');
-
-    if(storedUserLoggedinInformation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-  
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedin', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+  const ctx =  useContext(AuthContext);
 
   return (
-      <AuthContext.Provider 
-        value={{
-          isLoggedIn: isLoggedIn,
-        }}
-      >
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <React.Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-      </AuthContext.Provider>
+      </React.Fragment>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer  } from 'react';
+import React, { useState, useEffect, useReducer, useContext  } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../UI/store/auth-context';
+import Input from '../UI/Input/Input';
 
 const emailReducer = (state, action) => {
   if(action.type === 'user-input') {
@@ -43,6 +45,8 @@ const Login = (props) => {
     value: '',
     isValid: 'null',
   });
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('Effect Running');
@@ -95,40 +99,30 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
+        <Input 
+          id="email" 
+          label="E-Mail" 
+          type="email" 
+          isValid={emailIsValid} 
+          value={emailState.value} 
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+          <Input
             id="password"
+            label="Password"
+            type="password"
+            isValid={passwordIsValid}
             value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
-        </div>
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
